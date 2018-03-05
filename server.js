bb69e1523d5f54803b155d860719db8cc2a9edfc
Steven Inouye <steveninouye@msn.com>
2018-03-05 03:37:47 -1000

    finished displaying GET requests to browser

--- server.js
+++ server.js
@@ -1,25 +1,59 @@
 const http = require('http');
 const fs = require('fs');
 const qstring = require('querystring');
-let options = {};
+
+//define listening port
 let port = 8080;
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200);
-  res.write('<h1>hello</h1>');
-  //   console.log(req.headers);
-  //   console.log('path ', req.path);
-});
-server.on('request', (req, res) => {
-  const { method, url } = req;
-  //   console.log(req);
-  console.log(method);
-  console.log(url);
+//read public directory files
+let publicDirFiles;
+fs.readdir('./public/', (err, files) => {
+  publicDirFiles = files;
 });
 
-server.on('data', data => {
-  //   console.log(data);
-});
-server.listen(port, () => {
-  console.log(`server is running on port ${port}`);
-});
+//create server
+const server = http
+  .createServer((req, res) => {})
+  .on('request', (req, res) => {
+    const { method, url } = req;
+
+    //define local file path
+    let localFilePath;
+    //set default status HTML status code to 200
+    let HTMLStatusCode = 200;
+    if (url === '/') {
+      localFilePath = '/index.html';
+    } else if (url === '/favicon.ico') {
+      localFilePath = '/superman.png';
+    } else if (
+      publicDirFiles.indexOf(url.slice(1)) !== -1 ||
+      url.slice(-4) === '.css'
+    ) {
+      localFilePath = url;
+    } else {
+      localFilePath = '/404.html';
+      HTMLStatusCode = 404;
+    }
+
+    //define content type to input into response headers
+    let contentType;
+    if (localFilePath.slice(-5) === '.html') {
+      contentType = 'text/html';
+    } else if (localFilePath.slice(-4) === '.css') {
+      contentType = 'text/css';
+    } else if (localFilePath.slice(-4) === '.png') {
+      contentType = 'image/x-icon';
+    }
+
+    //read file from local file path in public directory
+    fs.readFile('./public' + localFilePath, function(err, data) {
+      //write the headers
+      res.writeHead(HTMLStatusCode, { 'Content-Type': contentType });
+      res.write(data);
+      res.end();
+    });
+  })
+  .on('data', data => {})
+  .listen(port, () => {
+    console.log(`server is running on port ${port}`);
+  });
